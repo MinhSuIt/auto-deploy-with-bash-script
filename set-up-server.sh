5507a6a8237b64e17c6616f24063f6a18eefad99
sudo <sudo@antking.asia>
2022-12-25 20:32:49 +0700

    a

--- set-up-server.sh
+++ set-up-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # bước set up ssh phải làm manual ko tự động đc
 # source set-up-ssh.sh
-
+# với GCP VPS khi ssh lần đầu vào phải thiết lập mật khẩu để làm các thao tác khác
+# sudo passwd
 source set-up-php.sh $2
 source set-up-nginx.sh $2
 source set-up-mysql.sh $2
--- set-up-user-automation.sh
+++ set-up-user-automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # thực hiện lệnh sudo mà ko cần trả lời password
-sudo useradd -m -p automation-user-password automation-user
+# sudo useradd -m -p automation-user-password automation-user
 sudo su <<EOF
-echo "automation-user ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "minhsudoit ALL=(ALL:ALL) ALL" >> /etc/sudoers
 EOF
