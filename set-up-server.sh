9205a95ffa7c9e8d516369978032d642bc494c38
sudo <sudo@antking.asia>
2022-12-25 23:53:04 +0700

    A

--- set-up-server.sh
+++ set-up-server.sh
@@ -2,13 +2,14 @@
 # với GCP VPS khi ssh lần đầu vào phải thiết lập mật khẩu để làm các thao tác khác
 # set password cho tài khoản root: sudo su rồi passwd
 # chuyển sang tài khoản root bằng: su sau đó tạo mật khẩu cho tài khoản thực thi script là minhsudoit: passwd minhsudoit
-# vào tài khoản minhsudoit chạy:
-# sudo su <<EOF
-# echo "minhsudoit ALL=(ALL) ALL" >> /etc/sudoers
-# EOF
+
 # Cách tự động password: https://www.baeldung.com/linux/passwd-shell-script
 
 # bước set up ssh phải làm manual ko tự động đc, trong set-up-ssh.sh
+# vào tài khoản root bằng lệnh su rồi chạy:
+sudo su <<EOF
+echo "minhsudoit ALL=(ALL) ALL" >> /etc/sudoers
+EOF
 source set-up-php.sh $2
 source set-up-nginx.sh $2
 source set-up-mysql.sh $2
